9266a987a77e390b5e83c28ce05d12a5f29149c1
henry cheche <henry.cheche@student.moringaschool.com>
2023-04-04 22:56:11 +0300

    Get elements using querySelector()

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get elements from index.html
+const registerForm = document.querySelector('#register');
+const resultForm = document.querySelector('#result');
+const rname = document.querySelector('#rname');
+const rheight = document.querySelector('#rheight');
+const rmass = document.querySelector('#rmass');
+const rage = document.querySelector('#rage');
+const rdate = document.querySelector('#rdate');
